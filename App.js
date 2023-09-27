5cc830f9b4eb3f499e2be7df6d69f61cdd175c3f
nntcuong <thecuongyt3htk@gmail.com>
2023-09-27 07:55:41 +0700

    useState list

--- App.js
+++ App.js
@@ -1,27 +1,56 @@
-import { View, Text, TextInput,Button, StyleSheet } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { useState } from "react";
+
 
 export default function App() {
+
+  const [value, setvalue] = useState("");
+  const [listNotes, setListNotes] = useState([]);
+
+  function onChangeTXT(getEnterText) {
+    setvalue(getEnterText)
+
+  }
+  function onPressBNT() {
+
+    setListNotes(currentNotes => [...currentNotes, value])
+  }
+  console.log(listNotes);
   return (
     <View style={{
       padding: 60
     }}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputVien} placeholder="Moi ban nhap"/>
-        <Button color={'#000'} title="Click"/>
+        <TextInput onChangeText={onChangeTXT} style={styles.inputVien} placeholder="Moi ban nhap" />
+        <Button onPress={onPressBNT} color={'#000'} title="Click" />
+      </View>
+
+      <View style={styles.listContainer}>
+        {
+          listNotes.map((item, index) => <Text key={'item${index}'}>{item}</Text>)
+        }
+
       </View>
     </View>
   )
 }
 
-const styles=StyleSheet.create({
-  inputContainer : {
-    flexDirection: 'row'
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    paddingBottom: 30,
+    borderBottomWidth: 1,
 
   },
-  inputVien : {
-    borderWidth : 1,
-    borderColor :'Black',
+  inputVien: {
+    borderWidth: 1,
+    borderColor: 'Black',
 
-    flex : 1 ,
+    flex: 1,
   },
+
+  listContainer: {
+    paddingTop: 30,
+
+  }
 })
